c3b715d31e02279e65d40f891117ebb348ec30f3
Sunny Bhagure <sunnybhagure64@gmail.com>
2026-05-29 21:52:52 +0530

    routes

--- Backend/routes/builderReview.js
+++ Backend/routes/builderReview.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-// ITHLI CHUK SUDHAR: 'protectUser' naav vapar
+
 const { protectUser } = require('../middleware/auth'); 
 const { addBuilderReview, getBuilderReviews, deleteBuilderReview,getUserBuilderReviews  } = require('../Controller/builderReviewController');
 
-// Ithe 'protectUser' pass kar
+
 router.post('/add', protectUser, addBuilderReview); 
 router.get('/:builderId', getBuilderReviews);
 router.delete('/:id', protectUser, deleteBuilderReview);
