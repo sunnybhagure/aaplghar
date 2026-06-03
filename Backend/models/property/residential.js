d5c6535cf4360c0a35b69a1b8b71980b8d886fa7
Sunny Bhagure <sunnybhagure64@gmail.com>
2026-06-03 22:57:15 +0530

    update routes

--- Backend/models/property/residential.js
+++ Backend/models/property/residential.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const residentialSchema = new mongoose.Schema({
   propertySubTypes: [String], 
-  // Dynamic keys handle karnyathi Mixed type vaprava lagel
+  
   config: { 
     type: mongoose.Schema.Types.Mixed 
   },
